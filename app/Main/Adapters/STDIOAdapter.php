4e3a9220229babe50719866343b6e842a731893d
brennomatheus <brenno_silva_@hotmail.com>
2020-08-06 15:24:57 -0300

    add STIOAdapter

---
+++ app/Main/Adapters/STDIOAdapter.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Main\Adapters;
+
+use App\Main\Factories\GetRoutesInfoControllerFactory;
+use App\Presentation\Protocols\Request;
+
+class STDIOAdapter
+{
+    public function run()
+    {
+        echo "Para sair digite 'quit'\n";
+        echo "Digite as rotas dos trens:\n";
+
+        while (true) {
+            $input = trim(fgets(STDIN));
+
+            if ($input === 'quit') die;
+
+            $inputs = preg_split("/, /", $input);
+
+            $controller = (new GetRoutesInfoControllerFactory())->make();
+            $response = $controller->handle(new Request($inputs));
+
+            if ($response->getStatusCode() === 200)
+                foreach ($response->getBody() as $key => $value) {
+                    $key = ++$key;
+                    $value = $value ?? 'NO SOUCH ROUTE';
+                    echo "Output #$key: $value\n";
+                }
+            else
+                echo "Houve erro ao executar o programa, tente novamente \n";
+
+            echo "Digite as rotas dos trens: \n";
+        }
+    }
+}
+
+//AB8, AC8, BE7, BC100, CA8, CB111, DA20, DB30, DC5, DE40, EB7, EA50
