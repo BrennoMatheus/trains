f000aebfb5e7578189c992ade1d20500ffdae6a8
brennomatheus <brenno_silva_@hotmail.com>
2020-08-06 15:23:18 -0300

    correction on validator

--- app/Presentation/Validators/Validator.php
+++ app/Presentation/Validators/Validator.php
@@ -6,17 +6,10 @@ use App\Presentation\Protocols\ValidatorInterface;
 
 class Validator implements ValidatorInterface
 {
-    private Validator $validator;
-
-    public function __construct(Validator $validator)
-    {
-        $this->validator = $validator;
-    }
-
     public function validate(array $data): bool
     {
         foreach ($data as $input)
-            if (!preg_match('/[A-E][A-E][1-9][0-9]*/', $input))
+            if (!preg_match('/^[A-E]{2}[1-9][0-9]*/', $input))
                 return false;
 
         return true;
